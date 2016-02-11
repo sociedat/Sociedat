c4b5eb07d77f8f74db11fe5e9e4af340a52ae886
viomac <vmaceda_29@hotmail.com>
2016-02-11 10:00:19 -0600

    Deleted some comments, commented scrip line to load parallax library and updated menubar.

--- blog.php
+++ blog.php
@@ -48,8 +48,8 @@
 						<li><a href="index.html#team">Equipo</a></li>
 						<li><a href="index.html#line-pricing">Membresías</a></li>
 						<li><a href="index.html#section-works">Redes Sociales</a></li>
-                        <li><a href="index.html#">Blog</a></li>
 						<li><a href="index.html#section-contact">Contacto</a></li>
+                        <li><a href="index.html#">Blog</a></li>
 					</ul>
 				</div><!--/.navbar-collapse -->
 			</div>
@@ -166,7 +166,7 @@
 	<script src="js/jquery.isotope.min.js"></script>
 	<script src="js/jquery.nicescroll.min.js"></script>
 	<script src="js/fancybox/jquery.fancybox.pack.js"></script>
-	<script src="js/jquery.parallax-1.1.3.js" type="text/javascript" ></script>
+	<!-- <script src="js/jquery.parallax-1.1.3.js" type="text/javascript" ></script> -->
 	<script src="js/skrollr.min.js"></script>		
 	<script src="js/jquery.scrollTo-1.4.3.1-min.js"></script>
 	<script src="js/jquery.localscroll-1.2.7-min.js"></script>
@@ -178,33 +178,6 @@
     <script src="js/main.js"></script>
         
     <script src="js/sociedat-custom.js"></script>
-        <!--<script type="text/javascript">
-            // When the window has finished loading create our google map below
-            google.maps.event.addDomListener(window, 'load', init);
-        
-            function init() {
-                // Basic options for a simple Google Map
-                // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-                var mapOptions = {
-                    // How zoomed in you want the map to start at (always required)
-                    zoom: 11,
-
-                    // The latitude and longitude to center the map (always required)
-                    center: new google.maps.LatLng(40.6700, -73.9400), // New York
-
-                    // How you would like to style the map. 
-                    // This is where you would paste any style found on Snazzy Maps.
-                    styles: [	{		featureType:"all",		elementType:"all",		stylers:[		{			invert_lightness:true		},		{			saturation:10		},		{			lightness:30		},		{			gamma:0.5		},		{			hue:"#1C705B"		}		]	}	]
-                };
-
-                // Get the HTML DOM element that will contain your map 
-                // We are using a div with id="map" seen below in the <body>
-                var mapElement = document.getElementById('map');
-
-                // Create the Google Map using out element and options defined above
-                var map = new google.maps.Map(mapElement, mapOptions);
-            }
-        </script> -->
 		 <script src="js/wow.min.js"></script>
 	 <script>
 	 wow = new WOW(
