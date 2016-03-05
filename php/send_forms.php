5141df9676952b296564298263ec33abca2df391
viomac <vmaceda_29@hotmail.com>
2016-03-04 22:06:05 -0600

    Testing body message to send mail.

--- php/send_forms.php
+++ php/send_forms.php
@@ -12,11 +12,47 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $to = "contacto@sociedat.org";
         $subject = "Solicitud de información de cursos SOCIEDAT";
         
-        send_form($to, $subject, $body);
+        //send_form($to, $subject, $body);
+        
+        require_once('class.phpmailer.php');
+        $mail = new PHPMailer();   
+        
+        //indico a la clase que use SMTP
+        $mail->IsSMTP();
+        //permite modo debug para ver mensajes de las cosas que van ocurriendo
+        $mail->SMTPDebug = 2;
+        //Debo de hacer autenticación SMTP
+        $mail->SMTPAuth = true;
+        $mail->SMTPSecure = "ssl";
+        //indico el servidor de Gmail para SMTP
+        $mail->Host = "smtp.sendgrid.net";
+        //indico el puerto que usa Gmail
+        $mail->Port = 465;
+        //indico un usuario / clave de un usuario de gmail
+        $mail->Username = "azure_ba1608611275c790d8b4945c590c8684@azure.com";
+        $mail->Password = "DopplerEffect2015";
+        $mail->SetFrom('azure_ba1608611275c790d8b4945c590c8684@azure.com', 'SoCieDat');
+        $mail->AddReplyTo("azure_ba1608611275c790d8b4945c590c8684@azure.com","SoCieDat");
+        $mail->Subject = "Prueba de Postal";
+
+   
+        $mail->MsgHTML($body);
+        //indico destinatario
+        $address = "contacto@sociedat.org";
+        $mail->AddAddress($address, "Nombre completo");
+        if(!$mail->Send()) {
+            echo "Error al enviar: " . $mail->ErrorInfo;
+        } else {
+            echo "Mensaje enviado!";
+        }
         
     }
-        
+     
+    /*
     function send_form($to, $subject, $body){
+        
+        echo "DENTRO de la función";
+        
         require_once('class.phpmailer.php');
         $mail = new PHPMailer();   
         
@@ -37,7 +73,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $mail->SetFrom('azure_ba1608611275c790d8b4945c590c8684@azure.com', 'SoCieDat');
         $mail->AddReplyTo("vmaceda29@gmail.com","SoCieDat");
         $mail->Subject = "Prueba de Postal";
-
+        
+        echo $body;
+        
         $mail->MsgHTML($body);
         //indico destinatario
         $address = "contacto@sociedat.org";
@@ -47,7 +85,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         } else {
         echo "Mensaje enviado!";
         }
-    }
+    } */
     
 }
 
