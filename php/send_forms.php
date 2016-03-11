59084db62313b81f0908a6675274f7b3c24b1d58
viomac <vmaceda_29@hotmail.com>
2016-03-11 14:11:38 -0600

    Make phpmailer sends email message using UTF-8.

--- php/send_forms.php
+++ php/send_forms.php
@@ -1,26 +1,47 @@
 <?php
+header ('Content-type: text/html; charset=utf-8');
 require_once('class.phpmailer.php');
+
+
+$begin = '
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" Content-Type:"text/html" charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>SOCIEDAT Sociedad de Científicos de Datos</title>
+        <meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <p>';
+$end = '
+        </p>
+    </body>
+</html>
+';
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     if(isset($_POST['getInfo'])){
-        echo '<p>Solicitud de información de cursos</p>';
+        echo 'Solicitud de información de cursos<br>';
         
-        $body =  "Nombre del solicitante: " . $_POST['userName']
+        $body = $begin . "Nombre del solicitante: " . $_POST['userName']
             . "<br>Email de contacto: " . $_POST['email']
             . "<br>Curso de interés: " . $_POST['requiredCourse']
             . "<br>Perfil de los estudiantes: " . $_POST['profile']
-            . "<br>Duración deseada: " . $_POST['duration'];
+            . "<br>Duración deseada: " . $_POST['duration'] . $end;
         $subject = "Solicitud de información de cursos SOCIEDAT";
         $file = "";
         send_form($subject, $body, $file);
     }
     
     if($_POST['sendMembership']){
-        echo '<p>Registro de membresía</p>';
+        echo 'Registro de membresía<br>';
         $file = $_FILES['payment']['tmp_name'];
         echo "RUTA del archivo: ".$file."<br><br>    ";
-        $body =  
-            "Datos del solicitante:"
+        $body =
+            $begin
+            ."Datos del solicitante:"
             ."<br>Nombre: ". $_POST['userFirstName']
             ."<br>Apellidos: ". $_POST['userLastName']
             ."<br>Fecha de nacimiento: ". $_POST['birth']
@@ -43,7 +64,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             ."<br>¿Te gustaría colaborar a que la ciencia de datos se difunda y crezca en México? Platícanos cómo: <br>"
             . $_POST['diffusionIdeas']
             ."<br>¿Te gustaría dar pláticas o conferencias? .... cuéntanos tus temas: <br>"
-            . $_POST['giveConferences'];
+            . $_POST['giveConferences']
+            . $end;
         
         $subject = "Solicitud de registro de nuevo miembro";
         send_form($subject, $body, $file);
@@ -54,7 +76,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 
 function send_form($subject, $body, $file){
-
+    
     $mail = new PHPMailer();   
 
     //indico a la clase que use SMTP
@@ -75,13 +97,14 @@ function send_form($subject, $body, $file){
     $mail->AddReplyTo("info@sociedat.org","SoCieDat");
     $mail->Subject = $subject;
 
-
+    
     $mail->MsgHTML($body);
     if($file)
         $mail->AddAttachment($file);
     //indico destinatario
     $address = "contacto@sociedat.org";
     $mail->AddAddress($address, "Sociedad de Científicos de Datos");
+    $mail->CharSet = 'UTF-8';
     if(!$mail->Send()) {
         echo "Error al enviar: " . $mail->ErrorInfo;
     } else {
